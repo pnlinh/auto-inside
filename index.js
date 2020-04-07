797166aaaf62dab30946b03d0c6743e04708c1bd
Ngoc Linh Pham <pnlinh1207@gmail.com>
2020-04-07 10:24:55 +0700

    update function

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const dotenv = require('dotenv');
+const config = dotenv.config();
 
-const url = 'https://inside.fptshop.com.vn/DefaultIntranet.aspx';
+const url = process.env.INSIDE_WEB_URL || 'https://inside.fptshop.com.vn';
 
 const account = {
-   username: '',
-   password: ''
+   username: process.env.INSIDE_CODE,
+   password: process.env.INSIDE_PASS,
 };
 
 async function start(url) {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
     await page.setViewport({width: 1366, height: 768});
 
@@ -24,17 +27,36 @@ async function start(url) {
         page.waitForNavigation({waitUntil: 'networkidle0'}),
     ]);
 
+    console.log('Login successfully');
+
     console.log(response);
 
     // Checkin
-    await page.click('#imgcheckin', {delay: 200});
+    try {
+        await page.click('#imgcheckin', {delay: 200});
+
+        console.log('Checkin OK');
+        fs.appendFileSync('logs.txt', `Checkin OK at: ${new Date} \r`);
+    } catch (err) {
+        //console.error(err);
+        console.log('Checkin error');
+        fs.appendFileSync('logs.txt', `Checkin error at: ${new Date} \r`);
+
+        await closeBrowser(browser.close(), 5000);
+    }
 
     // Log out
     //await page.click('#ctl00_LinkButton1', {delay: 200});
 
-    setTimeout(() => {
-        browser.close();
-    }, 1000);
+    await closeBrowser(browser.close());
+}
+
+function closeBrowser(callback, timer = 1000) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            return resolve(callback);
+        }, timer);
+    });
 }
 
 try {
